83874fd3a9f12a04f9b88a3ff5b95d6501abe0f2
Pedro Jesus <pedrojesus.cefet@gmai.com>
2021-12-08 19:52:15 -0300

    workaround to have a better error message

--- src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.ios.macos.cs
+++ src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.ios.macos.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CommunityToolkit.Maui.UI.Views;
 using Microsoft.Maui;
 
@@ -8,7 +9,7 @@ public static partial class NavigationExtensions
 {
 	static void PlatformShowPopup(BasePopup popup, IMauiContext mauiContext)
 	{
-		var popupNative = popup.ToHandler(mauiContext);
+		var popupNative = popup.ToHandlerInternal(mauiContext);
 		popupNative.Invoke(nameof(IBasePopup.OnOpened));
 	}
 
@@ -17,5 +18,34 @@ public static partial class NavigationExtensions
 		PlatformShowPopup(popup, mauiContext);
 
 		return popup.Result;
+	}
+}
+
+static class ExtensionsToDelete
+{
+	public static INativeViewHandler ToHandlerInternal(this IElement view, IMauiContext context)
+	{
+		_ = view ?? throw new ArgumentNullException(nameof(view));
+		_ = context ?? throw new ArgumentNullException(nameof(context));
+
+		//This is how MVU works. It collapses views down
+		if (view is IReplaceableView ir)
+			view = ir.ReplacedView;
+
+		var handler = view.Handler;
+		if (handler == null)
+			handler = context.Handlers.GetHandler(view.GetType());
+
+		if (handler == null)
+			throw new Exception($"Handler not found for view {view}.");
+
+		handler.SetMauiContext(context);
+
+		view.Handler = handler;
+
+		if (handler.VirtualView != view)
+			handler.SetVirtualView(view);
+
+		return (INativeViewHandler)handler;
 	}
 }

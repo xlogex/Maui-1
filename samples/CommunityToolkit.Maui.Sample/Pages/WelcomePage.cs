3d4f60d6c52d970764953f5f633bf9fd646d42d6
Vladislav Antonyuk <vlad.antonyuk@outlook.com>
2022-03-04 18:45:29 +0200

    DrawingNativeView iOS

--- samples/CommunityToolkit.Maui.Sample/Pages/WelcomePage.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/WelcomePage.cs
@@ -1,38 +1,24 @@
-﻿namespace CommunityToolkit.Maui.Sample.Pages;
+﻿using CommunityToolkit.Maui.Markup;
+
+namespace CommunityToolkit.Maui.Sample.Pages;
 
 public class WelcomePage : BasePage
 {
 	public WelcomePage()
 	{
-		//Content = new VerticalStackLayout
-		//{
-		//	Spacing = 12,
-
-		//	Children =
-		//	{
-		//		new Label { Text = "Welcome to the .NET MAUI Community Toolkit" }
-		//			.Font(size: 32).TextCenter(),
-
-		//		new Label { Text = "Explore features using the flyout menu in the top left" }
-		//			.Font(size: 16).TextCenter()
-		//	}
-		//}.CenterHorizontal();
-		Content = new VerticalStackLayout()
+		Content = new VerticalStackLayout
 		{
+			Spacing = 12,
+
 			Children =
 			{
-#if ANDROID
-				new CommunityToolkit.Maui.Views.DrawingView()
-				{
-					WidthRequest = 300,
-					HeightRequest = 300,
-					BackgroundColor = Colors.Yellow,
-					DefaultLineColor = Colors.Blue,
-					DefaultLineWidth = 10,
-				}
-#endif
+				new Label { Text = "Welcome to the .NET MAUI Community Toolkit" }
+					.Font(size: 32).TextCenter(),
+
+				new Label { Text = "Explore features using the flyout menu in the top left" }
+					.Font(size: 16).TextCenter()
 			}
-		};
+		}.CenterHorizontal();
 	}
 }
 
